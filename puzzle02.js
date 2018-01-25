ead575aa704289a43bf98bd679095adbd8e8207c
schu38 <sandy.chu.40@berkeley.edu>
2018-01-25 11:16:11 -0800

    Create puzzle02.js

---
+++ puzzle02.js
@@ -0,0 +1,31 @@
+function repeatDown() {
+  var count = 0;
+  while (count < 7) {
+    down ();
+    count = count + 1;
+  }
+}
+
+function repeatRight() {
+  var Rcount = 0;
+  while (Rcount < 2) {
+    right ();
+    Rcount = Rcount + 1;
+  }
+}
+
+function repeatUp() {
+  var Ucount = 0;
+  while (Ucount < 2) {
+    up ();
+    Ucount = Ucount + 1;
+  }
+}
+
+right();
+repeatDown();
+repeatRight();
+repeatUp();
+repeatUp();
+repeatRight();
+repeatDown();
