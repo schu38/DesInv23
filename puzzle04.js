d03aff4fd28eb1e0b3f8089488f12587acf08adf
schu38 <sandy.chu.40@berkeley.edu>
2018-01-25 11:33:38 -0800

    Update puzzle04.js

--- puzzle04.js
+++ puzzle04.js
@@ -1,1 +1,39 @@
 
+
+function repeatDown() {
+  var count = 0;
+  while (count < 2) {
+    down ();
+    count = count + 1;
+  }
+}
+
+function repeatRight() {
+  var Rcount = 0;
+  while (Rcount < 2) {
+    right ();
+    Rcount = Rcount + 1;
+  }
+}
+
+function repeatUp() {
+  var Ucount = 0;
+  while (Ucount < 2) {
+    up ();
+    Ucount = Ucount + 1;
+  }
+}
+
+repeatDown();
+repeatDown();
+right();
+var firstColorSeen = getColor();
+down();
+setColor(firstColorSeen);
+repeatRight();
+up();
+right()
+var sColorSeen = getColor();
+down();
+setColor(sColorSeen);
+repeatRight();
