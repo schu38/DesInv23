d3e165ec39eaf70ed3a754452cc9d7eaaf81d621
schu38 <sandy.chu.40@berkeley.edu>
2018-01-28 17:16:14 -0800

    Create puzzle09.js
    the display for this problem was weird, a tiny red asterisk that didn't have any bars going around it. I just made the repeat counts really huge and ran it until I got the message that I cleared the puzzle.

---
+++ puzzle09.js
@@ -0,0 +1,33 @@
+function repeatRight() {
+  var Rcount = 0;
+  while (Rcount < 100) {
+    right ();
+    Rcount = Rcount + 1;
+  }
+}
+
+function repeatUp() {
+  var Ucount = 0;
+  while (Ucount < 300) {
+      Ucount = Ucount + 1;
+  }
+}
+
+function repeatDown() {
+  var count = 0;
+  while (count < 100) {
+    down ();
+    count = count + 1;
+  }
+}
+
+function repeatLeft() {
+  var Lcount = 0;
+  while (Lcount < 100) {
+    Lcount = Lcount +1
+  }
+}
+
+
+repeatRight();
+repeatDown();
