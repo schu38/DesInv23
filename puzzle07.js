edee21a99ab6d8e5607ae234b15181bc19edb191
schu38 <sandy.chu.40@berkeley.edu>
2018-01-28 17:05:42 -0800

    Create puzzle07.js

---
+++ puzzle07.js
@@ -0,0 +1,39 @@
+function repeatRight() {
+  var Rcount = 0;
+  while (Rcount < 2) {
+    right ();
+    Rcount = Rcount + 1;
+  }
+}
+
+function repeatUp() {
+  var Ucount = 0;
+  while (Ucount < 8) {
+    up ();
+    if (getColor() === "red") {
+      repeatRight();
+    repeatDown() ;
+   	}
+    else 
+      Ucount = Ucount + 1;
+  }
+}
+
+function repeatDown() {
+  var count = 0;
+  while (count < 8) {
+    down ();
+    if (getColor() === "red") {
+      repeatRight();
+      repeatUp();
+    }
+    else
+    count = count + 1;
+  }
+}
+
+repeatDown();
+repeatDown();
+repeatDown();
+repeatDown();
+repeatDown();
